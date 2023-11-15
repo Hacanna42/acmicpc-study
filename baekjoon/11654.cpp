1ca3341e360a8c4ce6b815734ae783edd5a6b012
Hacanna42 <42440498+Hacanna42@users.noreply.github.com>
2023-11-15 22:32:45 +0900

    AllLineEquation Check, for modoo. need more logic updates. Nov 15.

---
+++ baekjoon/11654.cpp
@@ -0,0 +1,11 @@
+// 백준: 아스키 코드
+// https://www.acmicpc.net/problem/11654
+// 2023-11-15
+
+#include <iostream>
+int main() {
+    char temp;
+    std::cin >> temp;
+    int answer = temp;
+    std::cout << answer;
+}
