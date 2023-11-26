cb00fec7e44b088602e567c84c74b55efc4ec401
Hacanna42 <42440498+Hacanna42@users.noreply.github.com>
2023-11-27 00:09:46 +0900

    cat problem. 10171

---
+++ baekjoon/10171.cpp
@@ -0,0 +1,9 @@
+// 백준: 고양이
+// https://www.acmicpc.net/problem/10171
+// 2023-11-26
+
+#include <iostream>
+int main() {
+    std::cout << "\\    /\\\n )  ( ')\n(  /  )\n \\(__)|";
+    return 0;
+}
