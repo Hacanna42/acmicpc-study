3ff60eb6e57ded8b8a9e0018c1bd4ec75a896ab6
Hacanna42 <dtmdgus901@gmail.com>
2024-08-08 21:39:55 +0900

    Study log update: 2024-08-08 21:39

---
+++ baekjoon/BRONZE/25495.cpp
@@ -0,0 +1,15 @@
+// 백준: 에어팟
+// https://www.acmicpc.net/problem/25495
+// 2024-08-08
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N; // 에어팟 연결 횟수
+    cin >> N;
+    for (int i = 0; i < N; ++i) {
+    }
+    return 0;
+}
