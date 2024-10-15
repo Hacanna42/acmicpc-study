a6dec1d5e17dccd267342a3118b9c797b9dcdfdb
Hacanna42 <dtmdgus901@gmail.com>
2024-10-15 18:59:47 +0900

    Study log update: 2024-10-15 18:59

---
+++ baekjoon/BRONZE/32384.cpp
@@ -0,0 +1,17 @@
+// 백준: 사랑은 고려대입니다
+// https://www.acmicpc.net/problem/32384
+// 2024-10-15
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; ++i) {
+        cout << "LoveisKoreaUniversity ";
+    }
+    return 0;
+}
