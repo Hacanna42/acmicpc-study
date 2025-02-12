ab9a68fee8adcf77eb6b909088915ed060157a32
Hacanna42 <dtmdgus901@gmail.com>
2025-02-12 19:57:58 +0900

    Study log update: 2025-02-12 19:57

---
+++ baekjoon/SILVER/30804.cpp
@@ -0,0 +1,18 @@
+// 백준: 과일 탕후루
+// https://www.acmicpc.net/problem/30804
+// 2025-02-12
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N; // 과일의 개수 N
+    vector<int> tanghuru(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> tanghuru[i];
+    }
+
+    return 0;
+}
